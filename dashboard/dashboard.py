79d0480d7bfcc559cb4892f39279a6acef41fbcd
riofardinan <134182201+riofardinan@users.noreply.github.com>
2024-11-18 14:22:33 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import folium
 from folium.plugins import HeatMap
 from streamlit_folium import st_folium
 
